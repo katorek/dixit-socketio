ffe4c8f43bd14076eb78811f3a3eb2c246620458
katorek <wojtasj1996@gmail.com>
2021-01-02 14:56:46 +0100

    init repo

--- src/dixit-node-server/index.ts
+++ src/dixit-node-server/index.ts
@@ -1,7 +1,26 @@
-const express = require('express');
+import express from 'express';
+// rest of the code remains same
 const app = express();
 const PORT = 8000;
-app.get('/', (req,res) => res.send('Express + TypeScript Server'));
+
+const development = true;
+const redirectUrl = (development) ? 'http://localhost:4200' : 'http://dixit.katorek.ddns.net';
+
+
+app.get('/', (req, res) => {
+  res.send('' +
+    '<html lang="pl">' +
+    '  <head>' +
+    '    <meta http-equiv="Refresh" content="3; url=' + redirectUrl + '" />' +
+    '    <title>Dixit Online</title>  ' +
+    '  </head>' +
+    '  <body>' +
+    '    <p>Automatyczne przkierowanie w przeciągu <span id="countdown">3</span> sekund</p>' +
+    '  </body>' +
+    '</html>' +
+    '');
+});
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
